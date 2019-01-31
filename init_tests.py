ed95f63e703aab86235c2ecbd07fccf2f9d07fb6
Sten Nick <stennick01@gmail.com>
2019-01-31 15:49:27 +0300

    added fixture and decompose data

--- init_tests.py
+++ init_tests.py
@@ -1,38 +1,37 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.chrome.webdriver import WebDriver
 import pytest
 from parametrsGroup import Group
 from applcation import Application
 
 
-@pytest.fixture
+@pytest.fixture # спец. обоз. для инициал. фикстуры
 def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
-
-class test_group(unittest.TestCase):
-    def setUp(self):
-        self.app = Application
-        self.wd = WebDriver()
-        self.wd.maximize_window()
-        self.wd.implicitly_wait(60)
-
-    def test_test_getNewUser(self):
-        wd = self.wd
-        self.app.get_url(wd)
-        self.app.create_acount("Milena", "MilenaE@mail.ru")
-        time.sleep(3)
-        self.app.registration()
-        time.sleep(3)
-        self.app.add_user()
-        time.sleep(3)
-        self.app.add_information_about_user(Group(name="Milena22aq", email="Milena111W@yahoo.com", password="11Boby_Robinson11", gender="male",
-                                                  hobbi="writter", name_dog="milli", adress="Moscow", inn="984209854"))
-    def tearDown(self):
-        self.app.destroy()
+    fixture = Application() # тут переменной fixture дали значения модуля Application, где лежат основ. сценарии
+    request.addfinalizer(fixture.destroy) # вызываем функцию из Application с параметром destroy, которая завершает работу теста
+    return fixture # вернем переменную
 
 
-if __name__ == '__main__':
-    unittest.main()
+
+def test_test_getNewUser(app):
+    app.get_url()
+    app.create_acount("Alex Gagorin", "Gagorin@mail.ru")
+    app.registration()
+    app.add_user()
+    app.add_information_about_user(Group(name="Mikle Miller", email="MillerW@yahoo.com", password="11Boby_Miller1", gender="male",
+                                              hobbi="writter", name_dog="milli", adress="Moscow", inn="984209854"))
+
+
+"""
+пояснение:
+ - мы создали Фикстуру def app(request):
+    - объявили переменную и передали ей значение модуля Application, где у нас хранятся все тестовые функции
+    - параметру request.addfinalizer мы передали в качестве аргумента. вызов фикстуры (application.destroy())
+    - и вернули результат вызова фикстуры
+    
+    
+ - в test_test_getNewUser(app): мы передаем аргумент фиксутры (app) 
+   - далее уже без параметар self мы говорим откуда брать вызов функций, а берем из аргумента app 
+   - все функции вызываем из модуля Application
+   
+"""
 
